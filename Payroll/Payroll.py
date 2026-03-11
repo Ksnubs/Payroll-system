c093d3af9632d82c5efe3078782d7659b4f4d519
Ksnubs <leejiajing05@gmail.com>
2026-03-11 18:31:31 +0800

    add ot task function

--- Payroll/Payroll.py
+++ Payroll/Payroll.py
@@ -1,14 +1,9 @@
 #Payroll system
-
-#Employee List
-employee_list = [
-    {"name" : "Jia Jing",
-    "base" : 2000,
-    "task" : {}}
-]
-
 #Overtime rates
-ot_rates = {}
+ot_rates = {
+    "delivery" : 20,
+    "light sauce" : 2
+}
 
 #Employee class
 class Employee:
@@ -26,7 +21,11 @@ class Employee:
             total_pay += ( task_rate * count )
         
         return(total_pay)
-    
+
+#Employee List
+employee_list = [
+    Employee("Jia Jing", 3000, {})
+]
 #Program
 while True:
 
@@ -40,7 +39,7 @@ Select the corrosponding value to view:
 3. Overtime matters
 """)
 #User inputs in menu
-    choice = int(input("Enter the number: "))
+    choice = int(input("Enter the corrosponding Index: "))
 
     if choice == 0:
         break
@@ -48,10 +47,12 @@ Select the corrosponding value to view:
     elif choice == 1:
         print("\n----- Employee List -----")
         for emp in employee_list:
-            print("-" * 25)
-            for key, value in emp.items():
-                print(f"{key} | {value}")
-        exit = input("Press any key to continue: ")
+            print(f"Name: {emp.name}")
+            print(f"Base Salary: {emp.base}")
+            print(f"Tasks: {emp.task}")
+            print("-" * 10)
+
+        exit = input("\nPress any key to continue: ")
 
 #Adding new employee
     elif choice == 2:
@@ -59,17 +60,26 @@ Select the corrosponding value to view:
         new_emp_name = input("Enter the employee name: ")
         new_emp_base = int(input(f"Enter {new_emp_name} base salary: "))
 #New employee
-        new_emp = {"name" : new_emp_name,
-                   "base" : new_emp_base,
-                   "task" : {} }
-        employee_list.append(new_emp)
+        new_employee = Employee(new_emp_name, new_emp_base, {})
+        employee_list.append(new_employee)
         print(f"\n{new_emp_name} has been added to the system")
-        exit = input("Press any key to continue: ")
+        exit = input("\nPress any key to continue: ")
 
 #Adding Overtime task
     elif choice == 3:
         print("\n----- Adding employee OT -----")
         for index, emp in enumerate(employee_list):
-            name = emp["name"]
-            print(f"{index + 1}: {name}")
-        exit = input("To continue press any key: ")
+            name = emp.name
+            print(f"{index}: {name}")
+
+        emp_index = int(input("\nEnter employee's index: "))
+
+        print("----- List of task -----")
+        for index, task_name in enumerate(ot_rates):
+            print(f"{index}. {task_name}")
+        
+        task_index = int(input("\nEnter the task index: "))
+        task_key = list(ot_rates.keys())[task_index]
+        amount = int(input("Enter amount: "))
+#Adding to OT task to dictionary
+        employee_list[emp_index].task[task_key] = amount
