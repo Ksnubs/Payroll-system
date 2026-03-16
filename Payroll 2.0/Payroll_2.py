fdb9ab823c9fce4ff5fd7f4d66585198f1d9c38b
Ksnubs <leejiajing05@gmail.com>
2026-03-16 17:02:33 +0800

    functions

--- Payroll 2.0/Payroll_2.py
+++ Payroll 2.0/Payroll_2.py
@@ -1,11 +1,10 @@
 #Second version of Payroll System
-
 #Functions
 def get_user_input_int(max_int):
     while True:
         try:
             entry = int(input("Enter an Index: "))
-            if -1 < entry < max_int:
+            if -1 < entry <= max_int:
                 return(entry)
             else:
                 print("Please Enter a Valid Index.")
@@ -22,6 +21,18 @@ def title(title):
         sideline = int((space -1) /2)
         print("-" * sideline, f" {title} ", "-" * (sideline + 1))
 
+def exit():
+    input("Press any key to continue: ")
+#Classes
+class Employee:
+    def __init__(self, name, base_pay, task):
+        self.name = name
+        self.base_pay = base_pay
+        self.task = task
+
+employee_list = []
+    
+
 #WELCOME TITLE
 title("Welcome to Beluga HR system")
 
@@ -29,5 +40,37 @@ title("Welcome to Beluga HR system")
 while True:
     print("\nPlease select the action you want to access.")
     print("0. Close the System.")
+    print("1. Manage Staff.")
+    print("2. Manage Overtime Details.")
+    print("3. Edit Overtime Worked.")
+#Input for users choice
+    choice = get_user_input_int(3)
 
-   
+    if choice == 0:
+        title("Closing the System...")
+        exit()
+        break
+
+    if choice == 1:
+        title("Manage Staff")
+        print("1. Add an Employee.")
+        print("2. Remove an Employee.")
+        print("3. View list of Employees")
+        
+        option = get_user_input_int(3)
+        if option == 1:
+            title("Add an Employee")
+            name = input("New Employee Name: ")
+            base_pay = int(input("New Employee Base Pay: "))
+            new_emp = Employee(name, base_pay, {})
+            employee_list.append(new_emp)
+            print(f"\n{name} has been added to the system")
+            exit()
+        
+        elif option == 3:
+            for emp in employee_list:
+                title("Employee List")
+                print(f"Name: {emp.name}")
+                print(f"Base Salary: {emp.base_pay}")
+                     
+        
